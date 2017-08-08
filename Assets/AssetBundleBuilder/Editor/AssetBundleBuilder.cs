60f1d2431a55c634eab9b439f65c7890c1535dd7
Mogoson <mogoson@qq.com>
2017-08-08 20:20:40 +0800

    Add files via upload

---
+++ Assets/AssetBundleBuilder/Editor/AssetBundleBuilder.cs
@@ -0,0 +1,66 @@
+/*************************************************************************
+ *  Copyright (C), 2017-2018, Mogoson tech. Co., Ltd.
+ *  FileName: AssetBundleBuilder.cs
+ *  Author: Mogoson   Version: 1.0   Date: 8/4/2017
+ *  Version Description:
+ *    Internal develop version,mainly to achieve its function.
+ *  File Description:
+ *    Ignore.
+ *  Class List:
+ *    <ID>           <name>             <description>
+ *     1.       AssetBundleBuilder         Ignore.
+ *  Function List:
+ *    <class ID>     <name>             <description>
+ *     1.
+ *  History:
+ *    <ID>    <author>      <time>      <version>      <description>
+ *     1.     Mogoson     8/4/2017       1.0        Build this file.
+ *************************************************************************/
+
+namespace Developer.AssetBundleBuilder
+{
+    using System.IO;
+    using UnityEditor;
+    using UnityEngine;
+
+    /// <summary>
+    /// Editor for build asset bundle.
+    /// </summary>
+    public class AssetBundleBuilder : ScriptableWizard
+    {
+        #region Property and Field
+        [Tooltip("Path of save asset bundles.")]
+        public string path = "Assets/AssetBundles";
+
+        [Tooltip("Options of build asset bundles.")]
+        public BuildAssetBundleOptions options = BuildAssetBundleOptions.None;
+
+        [Tooltip("Target build platform.")]
+        public BuildTarget platform = BuildTarget.Android;
+        #endregion
+
+        #region Private Method
+        [MenuItem("Tool/AssetBundleBuilder &A")]
+        static void ShowEditor()
+        {
+            DisplayWizard("AssetBundleBuilder", typeof(AssetBundleBuilder), "Build");
+        }
+
+        void OnWizardUpdate()
+        {
+            if (path == string.Empty)
+                isValid = false;
+            else
+                isValid = true;
+        }
+
+        void OnWizardCreate()
+        {
+            if (Directory.Exists(path) == false)
+                Directory.CreateDirectory(path);
+            BuildPipeline.BuildAssetBundles(path, options, platform);
+            AssetDatabase.Refresh();
+        }
+        #endregion
+    }//class_end
+}//namespace_end
---
+++ Assets/AssetBundleBuilder/Readme.txt
@@ -0,0 +1,23 @@
+==========================================================================
+  Copyright (C), 2017-2018, Mogoson tech. Co., Ltd.
+  Name: AssetBundleBuilder
+  Author: Mogoson   Version: 1.0   Date: 8/4/2017
+==========================================================================
+  [Summeray]
+    This tool can be used to build asset bundle in Unity3D editor.
+--------------------------------------------------------------------------
+  [Environment]
+    This tool applies to Unity3D 5.0, .Net Framework 3.0 or above version.
+--------------------------------------------------------------------------
+  [Usage]
+    Define the bundles those you want to build by AssetBundle Menu at the
+    bottom of the asset Inspector window.
+    Find the menu item "Tool/AssetBundleBuilder" in Unity3D editor's menu
+    bar and click it to open the STEditor window or press combination key
+    Alt+A.
+    Set the "Path","Options" and "Platform" values.
+    Click the "Build" button to start building aseet bundles.
+--------------------------------------------------------------------------
+  [Contact]
+    If you have any questions, feel free to contact me at mogoson@qq.com.
+--------------------------------------------------------------------------
