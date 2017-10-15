79b879f21737de4ed4bf1ee6c1c23a8803884c46
Mogoson <mogoson@qq.com>
2017-10-15 09:03:40 -0500

    Format scripts.

--- Assets/AssetBundleBuilder/Editor/AssetBundleBuilder.cs
+++ Assets/AssetBundleBuilder/Editor/AssetBundleBuilder.cs
@@ -1,20 +1,14 @@
 /*************************************************************************
  *  Copyright (C), 2017-2018, Mogoson Tech. Co., Ltd.
- *  FileName: AssetBundleBuilder.cs
- *  Author: Mogoson   Version: 0.1.0   Date: 8/4/2017
- *  Version Description:
- *    Internal develop version,mainly to achieve its function.
- *  File Description:
- *    Ignore.
- *  Class List:
- *    <ID>           <name>             <description>
- *     1.       AssetBundleBuilder         Ignore.
- *  Function List:
- *    <class ID>     <name>             <description>
- *     1.
- *  History:
- *    <ID>    <author>      <time>      <version>      <description>
- *     1.     Mogoson      8/4/2017      0.1.0        Create this file.
+ *------------------------------------------------------------------------
+ *  File         :  AssetBundleBuilder.cs
+ *  Description  :  Config build options and build AssetBundles to
+ *                  target path.
+ *------------------------------------------------------------------------
+ *  Author       :  Mogoson
+ *  Version      :  0.1.0
+ *  Date         :  8/4/2017
+ *  Description  :  Initial development version.
  *************************************************************************/
 
 using System;
